e51160da25370b9cc5a6c78c4f0e1beb6206589d
Starior <zzzartem@gmail.com>
2021-10-31 23:20:54 +0300

    adaptive+js up to services

--- js/scripts.js
+++ js/scripts.js
@@ -12,3 +12,48 @@ function openMenu() {
     btnClose.className = "header__menu-button";
   }
 }
+
+function openContentAboutUs() {
+  var x = document.getElementById("about-us-content");
+  var btnOpen = document.getElementById("btnOpenAboutUs");
+  var btnClose = document.getElementById("btnCloseAboutUs");
+  if (x.className === "content-block__content") {
+    x.className += " unhide";
+    btnOpen.className = "arrow-expand hide";
+    btnClose.className = "arrow-rollup unhide";
+  } else {
+    x.className = "content-block__content";
+    btnOpen.className = "arrow-expand unhide";
+    btnClose.className = "arrow-rollup hide";
+  }
+}
+
+function openContentServices() {
+  var x = document.getElementById("services-content");
+  var btnOpen = document.getElementById("btnOpenServices");
+  var btnClose = document.getElementById("btnCloseServices");
+  if (x.className === "content-block__content") {
+    x.className += " unhide";
+    btnOpen.className = "arrow-expand hide";
+    btnClose.className = "arrow-rollup unhide";
+  } else {
+    x.className = "content-block__content";
+    btnOpen.className = "arrow-expand unhide";
+    btnClose.className = "arrow-rollup hide";
+  }
+}
+
+function openContentPrice() {
+  var x = document.getElementById("price-content");
+  var btnOpen = document.getElementById("btnOpenPrice");
+  var btnClose = document.getElementById("btnClosePrice");
+  if (x.className === "content-block__content") {
+    x.className += " unhide";
+    btnOpen.className = "arrow-expand hide";
+    btnClose.className = "arrow-rollup unhide";
+  } else {
+    x.className = "content-block__content";
+    btnOpen.className = "arrow-expand unhide";
+    btnClose.className = "arrow-rollup hide";
+  }
+}
